44b5f4aaab57d344013359975f4f7339bf384910
dzleok <68six8@gmail.com>
2024-07-08 17:14:53 +0300

    fix: eslint.config.js

--- task-1/eslint.config.js
+++ task-1/eslint.config.js
@@ -6,13 +6,21 @@ import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
   {
     rules: {
       'react/react-in-jsx-scope': 'off', // Turn off the default rule
+      ''
     },
   },
   {
